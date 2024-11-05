707e03a1420e3898c58088e35a1b271321a61ae7
Hennersxxx <henry.alex999@gmail.com>
2024-11-05 18:55:25 +0000

    Added the playRound function but have a 'Declaration or statement expected' error within the if-else-if statement

--- javascript.js
+++ javascript.js
@@ -27,3 +27,53 @@ function getHumanChoice()
         return choice
     }
 console.log(getHumanChoice())
+
+function playRound(humanChoice, computerChoice)
+    {
+        let humanChoice = (toLowerCase(getHumanChoice))
+        let computerChoice = getComputerChoice
+            if
+                (humanChoice = "rock") (and (computerChoice = "Rock"))
+                    {
+                        return "Draw!"
+                    }
+                        else if
+                            ((humanChoice = "rock") (and (computerChoice = "Paper")))
+                                {
+                                    return "You lose! Paper covers rock!"
+                                }
+                        else if
+                            ((humanChoice = "rock") (and (computerChoice = "Scissors")))
+                                {
+                                    return "You win! Rock beats scissors!"
+                                }
+                        else if
+                            ((humanChoice = "scissors") (and (computerChoice = "Paper")))
+                                {
+                                    return "You win! Scissors cuts paper!"
+                                }
+                        else if
+                            ((humanChoice = "scissors") (and (computerChoice = "Rock")))
+                                {
+                                    return "You lose! Rock beats scissors!"
+                                }
+                        else if
+                            ((humanChoice = "scissors") (and (computerChoice = "Scissors")))
+                                {
+                                    return "Draw!"
+                                }
+                        else if
+                            ((humanChoice = "paper") (and (computerChoice = "Paper")))
+                                {
+                                    return "Draw!"
+                                }
+                        else if
+                            ((humanChoice = "paper") (and (computerChoice = "Scissors")))
+                                {
+                                    return "You lose! Scissors cuts paper!"
+                                }
+                            else
+                                {
+                                    return "You win! Paper covers rock!"
+                                }
+    }
